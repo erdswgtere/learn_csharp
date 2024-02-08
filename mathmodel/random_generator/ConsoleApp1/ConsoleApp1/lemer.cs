7d34b8579387fe7d5d6709a3241abe8c513308fd
erdswgtere <maxxim200508@gmail.com>
2024-02-08 23:14:13 +0300

    Смешанный генератор

--- mathmodel/random_generator/ConsoleApp1/ConsoleApp1/lemer.cs
+++ mathmodel/random_generator/ConsoleApp1/ConsoleApp1/lemer.cs
@@ -6,34 +6,58 @@ namespace random_generator
         const int A = 5;
         int Mn;
         int Y;
-       public Lemer(int mn, int y)
+        public Lemer(int mn, int y)
         {
             Mn = mn;
             Y = y;
         }
-        double Rnd() {
+        double Rnd()
+        {
             Y = (A * Y) % Mn;
             return (double)Y / Mn;
         }
-       readonly double[] x = new double[N];
+        readonly double[] x = new double[N];
         public void Output()
         {
             for (int i = 0; i < N; i++)
             {
                 x[i] = Rnd();
             }
-            for(int i = 0;i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
-               Console.WriteLine($"{x[i]:f3}");
+                Console.WriteLine($"{x[i]:f3}");
             }
             Console.WriteLine();
         }
     }
     class Combined_Generator
     {
+        const int N = 1500;
+        const int A = 165;
+        const int Mu = 3463;
+        const int Mm = 4096 * 4;
+        int Y = 3887;
         public Combined_Generator()
         {
 
+        }
+        double Rnd()
+        {
+            Y = (A * Y + Mu) % Mm;
+            return (double)Y / Mm;
+        }
+        readonly double[] x = new double[N];
+        public void Output()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                x[i] = Rnd();
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{x[i]:f3}");
+            }
+            Console.WriteLine();
         }
     }
 }
