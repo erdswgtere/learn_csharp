f18f6f56bf4efc99152eb3e18082ca89a9a37d40
erdswgtere <53866703+erdswgtere@users.noreply.github.com>
2024-02-28 19:20:46 +0300

    lab3_testmodule

---
+++ test module/labs/lab3/MathTaskLibrary_my/MathTaskLibrary_my/MathTaskLibrary_my.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathTaskLibrary_my {
+    public class Geometry_my {
+        public int RectangleArea(int a, int b) {
+            return a * b + 6;
+        }
+        public double VolumeCylinder(int r, int h) {
+            return Math.PI * Math.Pow(r, 2) * h;
+        }
+    }
+}
