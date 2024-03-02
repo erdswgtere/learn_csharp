ae056bed47ece5dfab66e8959135b72dfb059033
erdswgtere <53866703+erdswgtere@users.noreply.github.com>
2024-03-02 17:57:51 +0300

    шаблон дислина

---
+++ шаблоны/gistogramma/Program.cs
@@ -0,0 +1,103 @@
+﻿using dislin_my;
+using random_generators;
+namespace Mult_RND_test {
+    class Program {
+        // Количество случайных чисел (размер массива values)
+        const int _N = 1500;
+        // Параметр мультипликативного датчика
+        static int _A; // Модуль 
+        static int _Mm;
+        // Случайное число
+        static int _Y;
+        // Число интервалов разбиения
+        static int _K;
+        /// <summary> Генератор случайных чисел
+        /// </summary> 
+        /// <returns> Случайное значение </returns> 
+        static double Rnd() {
+            _Y = (_A * _Y) % _Mm;
+            return (double)_Y / _Mm;
+        }
+        static Combined_Generator cmd = new Combined_Generator(_Y);
+        /// <summary> формировать набор данных 
+        /// </summary> 
+        /// <param name = "parValues"> Mассив случайных
+        /// чисел </param>
+        static void GenerateData(out double[] parValues) {
+            parValues = new double[_N];
+            // получение случайных чисел
+            for (int i = 0; i < _N; i++)
+                parValues[i] = cmd.Rnd(); //для примера вставлен комбинированный генератор
+        }
+        /// <summary> Получить массивы оценок для плотности и функции
+        /// pаспределения </summary>
+        /// <param name = "parValues"> Массив случайных чисел </param>
+        /// <param name = "parDataPlot"> Mассив частот </param>
+        /// <pаram name = "parDataFunc"> Mассив функции распределения </param>
+        /// <param name = "parMin"> Левая граница интервала </param>
+        /// <param name = "parMax"> Правая граница интервала </param> 
+        static void MakeData(double[] parValues, out double[] parDataPlot, out double[] parDataFunc, double parMin, double parMax) {
+            double delta = (parMax - parMin) / _K;
+            parDataPlot = new double[_K];
+            parDataFunc = new double[_K];
+            for (int i = 0; i < _N; i++) {
+                int j = (int)((parValues[i] - parMin) / delta);
+                if (j >= _K) j = _K - 1;
+                else if (j < 0) j = 0;
+                parDataPlot[j]++;
+            }
+            for (int i = 0; i < _K; i++)
+                parDataPlot[i] /= _N;
+            parDataFunc[0] = parDataPlot[0];
+            for (int i = 1; i < _K; i++)
+                parDataFunc[i] = parDataFunc[i - 1] + parDataPlot[i];
+            Console.WriteLine($"Кол-во элементов {parValues.Length}");
+        }
+        /// <summary> Получить статистические оценки</summary> 
+        /// <param name = "parValues"> Случайные числа
+        /// <param name = "parMx"> Mатематическое ожидание</param>
+        /// <раrаm пame = "раrS2"> Исправленная выборочная дисперсия </раrаm>
+        static void Estimate(double[] parValues, out double parMx, out double parS2) {
+            double m2 = 0;
+            parMx = 0;
+            for (int i = 0; i < _N; i++) {
+                parMx += parValues[i];
+                m2 += parValues[i] * parValues[i];
+            }
+            parMx /= _N;
+            m2 /= _N;
+            parS2 = (m2 - parMx * parMx) * _N / (_N - 1);
+        }
+        /// <summary> Основная программа </summary>
+        /// <param name = "args"> Аргументы </param> 
+        static void Main(string[] args) {
+            double[] values, dataPlot, dataFunc;
+            _Mm = 4096;
+            _A = 5;
+            _Y = _Mm - 5;
+            _K = 15; // размер массива частот
+            GenerateData(out values);
+            MakeData(values, out dataPlot, out dataFunc, 0.0, 1.0);
+            for (int i = 0; i < 15; i++) {
+                Console.WriteLine($"{(dataPlot[i] * 1000)} ");
+            }
+            Console.WriteLine($"Размерность {dataPlot.Length}");
+            Console.WriteLine();
+            for (int i = 0; i < 15; i++) {
+                Console.WriteLine($"{values[i] * 100} ");
+            }
+            //... построение гистограммы частот и 
+            // статистической функции распределения
+
+
+            Dislin_my dismy = new Dislin_my(ref dataPlot, ref values);
+            dismy.Diag();
+
+            Estimate(values, out double mx, out double dx);
+            Console.WriteLine();
+
+            // обработка статистических параметров
+
+        }
+    }
+}
---
+++ шаблоны/gistogramma/dislin_my.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace dislin_my {
+    public class Dislin_my {
+        static float[] chas;
+        static float[] randvalues;
+        public Dislin_my(ref double[] Chass, ref double[] Values) {
+            float[] floatChass = new float[Chass.Length];
+            for (var i = 0; i < Chass.Length; ++i)
+                floatChass[i] = (float)Chass[i] * 1000;
+
+            float[] floatValues = new float[Values.Length];
+            for (var i = 0; i < Values.Length; ++i) {
+                floatValues[i] = (float)Values[i] * 100;
+            }
+
+
+            chas = floatChass;
+            randvalues = floatValues;
+        }
+        public void Diag() {
+            int nya = 2700;
+
+            string ctit = "Bar Graphs (BARS)";
+            StringBuilder cbuf = new StringBuilder(25);
+
+            /* float[] x = { 0.3f, 10.0f, 20.0f, 30.0f, 40.0f, 50.0f, 60.0f, 70.0f, 80.0f, 90.0f, 100.0f }; */
+            float[] y = { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+
+            dislin.scrmod("revers");
+            dislin.setpag("da4p");
+            dislin.metafl("cons");
+            dislin.disini();
+            dislin.pagera();
+            dislin.complx();
+            dislin.ticks(5, "x");
+            dislin.intax();
+            dislin.axslen(1800, 1000);
+            dislin.titlin(ctit, 1);
+
+            dislin.barwth(0.3f);
+            dislin.barbor(490);
+            dislin.legini(cbuf, 1, 25);
+            dislin.leglin(cbuf, "Combined_generator", 1);
+            dislin.legtit(" ");
+            dislin.shdpat(100);
+            dislin.axspos(200, nya - 2 * 500);
+            dislin.graf(0.0f, 100.0f, 0.0f, 5.0f, 0.0f, 100.0f, 0.0f, 10.0f);
+            dislin.color("blue");
+            dislin.bars(randvalues, y, chas, 15);
+            dislin.color("fore");
+            dislin.reset("bargrp");
+            dislin.height(50);
+            dislin.title();
+            dislin.legend(cbuf, 7);
+            dislin.endgrf();
+            dislin.disfin();
+        }
+    }
+}
