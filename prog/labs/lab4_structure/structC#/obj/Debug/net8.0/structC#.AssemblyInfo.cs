48f2e6e240d587c9fbff6a48955b45120aac15b5
erdswgtere <53866703+erdswgtere@users.noreply.github.com>
2024-03-05 19:21:23 +0300

    cash

--- prog/labs/lab4_structure/structC#/obj/Debug/net8.0/structC#.AssemblyInfo.cs
+++ prog/labs/lab4_structure/structC#/obj/Debug/net8.0/structC#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("structC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae056bed47ece5dfab66e8959135b72dfb059033")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc726f992101aef216b4a20b6f4a9296b6d2f6ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("structC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("structC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
