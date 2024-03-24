36989fceb0d5c7170cd2dde5ac15ef9638dfea8b
erdswgtere <maxxim200508@gmail.com>
2024-03-24 20:51:11 +0300

    коллекция(словарь)

--- prog/labs/lab5_collection_with_struct/structC#/Program.cs
+++ prog/labs/lab5_collection_with_struct/structC#/Program.cs
@@ -1,80 +1,12 @@
 ﻿namespace Health_and_Medicine;
-using System.Globalization;
+
 internal class Program {
     private static void Main(string[] args) {
         Console.Write("Введите кол-во аптек: ");
-        int n = Convert.ToInt32(Console.ReadLine());
-        List<Health> alllabor = new List<Health>(n);
-        List<Health.Medicine> all = new List<Health.Medicine>();
-        for (int i = 0; i < n; i++) {
-            Console.Write($"Введите ФИО сотрудника {i}: ");
-            string FIO = Convert.ToString(Console.ReadLine()!);
-            Console.Write("Должность: ");
-            string Post = Convert.ToString(Console.ReadLine()!);
-            Console.Write("Введите стаж работника(в годах): ");
-            int Staz = Convert.ToInt32(Console.ReadLine()!);
-            Console.Write("Дата приема на работу (в формате дд/мм/гггг слитно, знак / используется как разделитель): ");
-            DateTime dt1 = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
-            Labor labor1 = new Labor(FIO, Post, dt1, Staz);
-            alllabor.Add(labor1);
-            Console.WriteLine();
-        }
-        Console.WriteLine($"============");
-        foreach (Labor x in alllabor) {
-            x.Output();
-            Console.WriteLine();
-        }
-        Console.WriteLine($"============");
-
-        Console.Write("Хотите ли вы вывести инфромацию о всех работниках?(1 или 0 на ввод): ");
-        int t = Convert.ToInt32(Console.ReadLine()!);
-        switch (t) {
-            case 1:
-                foreach (Labor x in alllabor) {
-                    x.Output_ALL();
-                }
-                break;
-            case 0:
-                break;
-            default:
-                Console.WriteLine("Некоректный ввод");
-                break;
-        }
-        Console.Write("Хотите ли вы вывести информацию о конкретных работниках?(1 или 0 на ввод):");
-        t = Convert.ToInt32(Console.ReadLine()!);
-        switch (t) {
-            case 1:
-                List<int> labornumbers = new List<int>(n);
-                Console.Write("Введите номера работников информацию о которых вы хотете вывести(первый сотрудник под номером 0)\n" +
-                              "При вводе кол-ва чисел большего чем кол-во работников будет ошибка\n" +
-                              "Введите -1 если хотите закончить ввод ");
-
-                Console.WriteLine();
-                for (int i = 0; i != n; i++) {
-                    Console.Write("Введите номер работника: ");
-                    int d = Convert.ToInt32(Console.ReadLine()!);
-                    if (d < n && d != -1) {
-                        labornumbers.Add(d);
-                    }
-                    else {
-                        Console.WriteLine("Вы ввели число выходящее за кол-во работников или ввели -1 для завершения");
-                        break;
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine($"============");
-                for (int i = 0; i < labornumbers.Count; i++) {
-                    alllabor[labornumbers[i]].Output_ALL();
-                    Console.WriteLine();
-                }
-                break;
-            case 0:
-                Console.WriteLine("Программа завершена");
-                break;
-            default:
-                Console.WriteLine("Некоректный ввод");
-                break;
-        }
-
+        int n = int.Parse(Console.ReadLine()!);
+        Dictionary<string, List<Health.Medicine>> Health_Base = new Dictionary<string, List<Health.Medicine>>(n);
+        Health hlt = new Health(Health_Base, n);
+        hlt.input_in_base();
+        hlt.output_from_base();
     }
 }
--- prog/labs/lab5_collection_with_struct/structC#/mystruct.cs
+++ prog/labs/lab5_collection_with_struct/structC#/mystruct.cs
@@ -1,30 +1,77 @@
+using System.Globalization;
+
 namespace Health_and_Medicine {
     class Health {
-        string addres;
-        public Health(string addres) {
-            this.addres = addres;
-        }
+        Dictionary<string, List<Medicine>> HealthBase;
+        int n;
         public struct Medicine {
             string full_name;
             DateTime srok; //когда истекает срок годности
             int kolichestvo;
-            public Medicine(string full_name, bool nalichye, DateTime srok, int kolichestvo) {
+            public Medicine(string full_name, DateTime srok, int kolichestvo) {
                 this.full_name = full_name;
                 this.srok = srok;
                 this.kolichestvo = kolichestvo;
             }
-            void Nalichye_nagladno(){
+            void Nalichye_nagladno() {
                 if (kolichestvo > 0)
                     Console.WriteLine($"лекарство есть в наличии, его количество равно {kolichestvo}");
                 else
-                    Console.WriteLine("Данного лекарства нет в наличии"); 
+                    Console.WriteLine("Данного лекарства нет в наличии!");
             }
             public void Output_ALL() {
                 Console.WriteLine($"Полное имя = {full_name}");
-                Console.WriteLine($"Наличие данного лекарства = {Nalichye_nagladno}");
-                Console.WriteLine($"Дата  = {srok.ToShortDateString()}");
-                Console.WriteLine($"Кол-во препаратов  = {kolichestvo}");
+                Nalichye_nagladno();
+                Console.WriteLine($"Дата = {srok.ToShortDateString()}");
             }
+        }
+        public Health(Dictionary<string, List<Medicine>> HealthBase, int n) {
+            this.HealthBase = HealthBase;
+            this.n = n;
+        }
+        public void input_in_base() {
+            for (int i = 0; i < n; i++) {
+                Console.Write($"Введите адрес аптеки №{i + 1}: ");
+                string apt = Console.ReadLine()!;
+                Console.Write($"Введите кол-во лекарств в аптеке №{i + 1}: ");
+                int kol = int.Parse(Console.ReadLine()!);
+                List<Medicine> medicines = new List<Medicine>();
+                for (int j = 0; j < kol; j++) {
+                    Console.Write($"Введите название лекарства: ");
+                    string name = Console.ReadLine()!;
+                    Console.Write("Дата окончания срока годности лекарства (в формате дд/мм/гггг слитно, знак / используется как разделитель): ");
+                    DateTime dt1 = DateTime.ParseExact(Console.ReadLine()!, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    Console.Write("Кол-во лекарства: ");
+                    int kol_onelek = int.Parse(Console.ReadLine()!);
+                    Medicine leks = new Medicine(name, dt1, kol_onelek);
+                    medicines.Add(leks);
+                    Console.WriteLine("#########");
+                }
+                HealthBase.Add(apt, medicines);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        public void output_from_base() {
+            Console.WriteLine($"============");
+            Console.WriteLine("Введите номер(цифру) аптеки, для получения информации по ней");
+            Console.WriteLine("СПИСОК ДОСТУПНЫХ АПТЕК:");
+            for (int i = 0; i < HealthBase.Count; i++) {
+                Console.WriteLine($"Аптека №{i + 1} ");
+            }
+            Console.WriteLine();
+            Console.Write("Введите номер: ");
+            int num = int.Parse(Console.ReadLine()!);
+            List<string> myKeys = HealthBase.Keys.ToList();
+            if (HealthBase.TryGetValue(myKeys[num - 1], out List<Medicine> hlth)) {
+                for (int i = 0; i < hlth.Count; i++) {
+                    hlth[i].Output_ALL();
+                }
+            }
+            else {
+                Console.WriteLine("Информация о лекарствах аптеки не найдена");
+            }
+            Console.WriteLine($"============");
         }
 
     }
