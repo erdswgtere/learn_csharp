ee6220e42f3e1fd81699d3806d05c77b9413aa79
erdswgtere <maxxim200508@gmail.com>
2024-03-26 16:08:17 +0300

    pr2 test_module

---
+++ test module/pr/pr2/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace prr2 {
+    class Firma {
+        string name_firm{
+            get => name_firm;
+            set => name_firm = value;
+        }
+        class Otedl_1: Firma { 
+            class Sotr: Otedl_1 {
+
+            }
+            class Stat_sotr:Otedl_1 { 
+            
+            }
+        }
+    }
+}
---
+++ test_for_me/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp1 {
+    class MyException : Exception {
+
+        private int detail;
+        public MyException(int a) {
+            detail = a;
+        }
+        public string toString() {
+            return "MyException!" + detail;
+        }
+    }
+    class MultiNest {
+        static void procedure() {
+            try {
+                int[] c = { 1 };
+                c[42] = 99;
+            }
+            catch (IndexOutOfRangeException e) {
+                Console.WriteLine("array index oob: " + e);
+            }
+        }
+        public static void main(string[] args) {
+            try {
+                int a = args.Length;
+                Console.WriteLine("a = " + a);
+                int b = 42 / a;
+                procedure();
+            }
+            catch (ArithmeticException e) {
+                Console.WriteLine("div by 0: " + e);
+            }
+        }
+    }
+    class ExceptionDemo {
+        static void compute(int a) {
+            Console.WriteLine("Вызван compute(" + a + ")");
+            if (a > 10)
+                throw new MyException(a);
+            Console.WriteLine("Нормальное завершение");
+        }
+        static void Main(string[] args) {
+            try {
+                compute(1);
+                compute(20);
+            }
+            catch (MyException e) {
+                Console.WriteLine("Перехвачено " + e);
+            }
+        }
+    }
+}
