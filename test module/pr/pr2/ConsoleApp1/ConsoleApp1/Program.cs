0c871379b5247d0bee0ce7c005880144e3493263
erdswgtere <maxxim200508@gmail.com>
2024-03-27 22:23:16 +0300

    pr2 testmodule end

--- test module/pr/pr2/ConsoleApp1/ConsoleApp1/Program.cs
+++ test module/pr/pr2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,59 @@
 ﻿using prr2;
-
 namespace ConsoleApp1 {
+
     internal class Program {
         static void Main(string[] args) {
-            Firma frm = new Firma();
-            frm.Otedl_1
+            while (true) {
+                Console.WriteLine("СОТРУДНИК: ");
+                try {
+                    Sotr sotr = new Sotr();
+                    Console.Write("Введите фио сотрудника: ");
+                    sotr.fullname = Console.ReadLine()!;
+                    Console.Write("Введите должность сотрудника: ");
+                    sotr.place_in_ierachy = Console.ReadLine()!;
+                    Console.Write("Введите оклад сотрудника: ");
+                    sotr.oklad = int.Parse(Console.ReadLine()!);
+                    Console.WriteLine($"Зарплата сотрудника равна {sotr.Raschet()}");
+                    break;
+                }
+                catch (OkladException ex) {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                    
+                }
+                catch (FormatException) {
+                    Console.WriteLine("Введён неверный формат");
+                    continue;
+                }
+            }
+            Console.WriteLine("ШТАТНЫЙ СОТРУДНИК: ");
+            while (true) {
+                try {
+                    Stat_sotr stat_Sotr = new Stat_sotr();
+                    Console.Write("Введите фио сотрудника: ");
+                    stat_Sotr.fullname = Console.ReadLine()!;
+                    Console.Write("Введите должность сотрудника: ");
+                    stat_Sotr.place_in_ierachy = Console.ReadLine()!;
+                    Console.Write("Введите оклад сотрудника: ");
+                    stat_Sotr.oklad = int.Parse(Console.ReadLine()!);
+                    Console.Write("Введите премию сотрудника: ");
+                    stat_Sotr.prem = int.Parse(Console.ReadLine()!);
+                    Console.WriteLine($"Зарплата сотрудника равна {stat_Sotr.Raschet()}");
+                    break;
+                }
+                catch (OkladException ex) {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                catch (PremiyaException ex) {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                catch (FormatException) {
+                    Console.WriteLine("Введён неверный формат");
+                    continue;
+                }
+            }
         }
     }
 }
---
+++ test module/pr/pr2/ConsoleApp1/ConsoleApp1/pr2.cs
@@ -0,0 +1,95 @@
+﻿namespace prr2 {
+    class Firma {
+        string Name_firm;
+        public string name_firm {
+            get => Name_firm;
+            set => Name_firm = value;
+        }
+
+    }
+    class Otedl_1 : Firma {
+        string Name_otedl;
+        int Kol_sotr;
+        public string name_otedl {
+            get => Name_otedl;
+
+            set => Name_otedl = value;
+        }
+        public int kol_sotr {
+            get => Kol_sotr;
+            set {
+                if (value < 0) {
+                    throw new LaborException("Кол-во сотрудников не может быть отрицательным!");
+                }
+                else {
+                    Kol_sotr = value;
+                }
+            }
+        }
+    }
+    class Sotr : Otedl_1 {
+        string Fullname;
+        string Place_in_ierachy;
+        int Oklad;
+        public string fullname {
+            get => Fullname;
+            set {
+                Fullname = value;
+            }
+        }
+        public string place_in_ierachy {
+            get => Place_in_ierachy;
+            set => Place_in_ierachy = value;
+        }
+        public int oklad {
+            get => Oklad;
+            set {
+                if (value < 0) {
+                    throw new OkladException("Невозможно создать сотрудника – указан отрицательный оклад ", value);
+                }
+                else {
+                    Oklad = value;
+                }
+            }
+        }
+        public double Raschet() {
+            return Oklad * 21;
+        }
+    }
+    class Stat_sotr : Sotr {
+        int Prem;
+       public int prem {
+            get => Prem;
+            set => Prem = value;
+        }
+        public double Raschet() {
+            double a = oklad * 21 + prem;
+            if (prem < 0) {
+                throw new PremiyaException("Премия не может быть отрицательна!");
+            }
+            else {
+                return a;
+            }
+        }
+    }
+
+    class PremiyaException : Exception {
+
+        public PremiyaException(string message) : base(message) {
+
+        }
+    }
+    class OkladException : Exception {
+
+        public OkladException(string message, int oklad) : base(message) {
+
+            Console.WriteLine($"Оклад равен {oklad}");
+
+        }
+    }
+    class LaborException : Exception {
+
+        public LaborException(string message) : base(message) {
+        }
+    }
+}
