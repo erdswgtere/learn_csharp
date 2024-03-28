2cd5dede6e23891cd18349a51fdc598b8eb14b5e
erdswgtere <maxxim200508@gmail.com>
2024-03-28 18:35:41 +0300

    mathmodel lab3

---
+++ mathmodel/lab3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Введите кол-во испытаний");
+            int n = int.Parse(Console.ReadLine()!);
+            Console.Write("Введите число шагов");
+            int m = int.Parse(Console.ReadLine()!);
+            Random random = new Random();
+            double L = 0;
+            for (int i = 0; i < n; i++) {
+                x = у = 0;
+                for (int j = 0; j < m; j++) {
+                    double r = random.NextDouble();
+                    if (r < 0.25) x++;
+                    else if (r < 0.5) x--;
+                    else if (r < 0.75) y--;
+                    else y++;
+                }
+                L += Math.Sqrt(x * x + у * y);
+            }
+            L /= n;
+            //...
+
+        }
+    }
+}
