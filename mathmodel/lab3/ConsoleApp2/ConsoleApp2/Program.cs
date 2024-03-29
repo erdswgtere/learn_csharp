9325bdd0db358089af298ceaed0109092947b5ec
erdswgtere <maxxim200508@gmail.com>
2024-03-29 14:31:57 +0300

    mathmodel

--- mathmodel/lab3/ConsoleApp2/ConsoleApp2/Program.cs
+++ mathmodel/lab3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,26 +1,63 @@
 ﻿namespace ConsoleApp2 {
     internal class Program {
         static void Main(string[] args) {
-            Console.Write("Введите кол-во испытаний");
+            Console.Write("Введите количество пчёл:");
+            int kol = int.Parse(Console.ReadLine()!);
+            List<Bee> bees = new List<Bee>(kol);
+            Console.Write("Введите кол-во испытаний: ");
             int n = int.Parse(Console.ReadLine()!);
-            Console.Write("Введите число шагов");
+            Console.Write("Введите число шагов: ");
             int m = int.Parse(Console.ReadLine()!);
-            Random random = new Random();
-            double L = 0;
-            for (int i = 0; i < n; i++) {
-                x = у = 0;
-                for (int j = 0; j < m; j++) {
-                    double r = random.NextDouble();
-                    if (r < 0.25) x++;
-                    else if (r < 0.5) x--;
-                    else if (r < 0.75) y--;
-                    else y++;
-                }
-                L += Math.Sqrt(x * x + у * y);
+            for (int i = 0; i < kol; i++) {
+                Bee bee_one = new Bee(n, m);
+                bees.Add(bee_one);
             }
-            L /= n;
-            //...
-
+            double L_from_all_bees = 0;
+            for(int i = 0; i < kol; i++) {
+                Console.WriteLine($"Среднее смещение пчелы № {i + 1} равно {bees[i].bluzd()}");
+                L_from_all_bees += bees[i].bluzd();
+            }
+            Console.WriteLine($"Среднее смещение всего роя равно {L_from_all_bees / kol}");
         }
+        struct Bee {
+            int n;
+            int m;
+            public Bee(int n, int m) {
+                this.n = n;
+                this.m = m;
+            }
+            public double bluzd() {
+                Random random = new Random();
+                double L = 0;
+                double P = 0;
+                for (int i = 0; i < n; i++) {
+                    int y;
+                    int x = y = 0;
+                    for (int j = 0; j < m; j++) {
+                        double r = random.NextDouble();
+                        if (r < 0.25) x++;
+                        else if (r < 0.5) x--;
+                        else if (r < 0.75) y--;
+                        else y++;
+
+                    }
+                    L += Math.Sqrt(x * x + y * y);
+                    P += x + y;
+                }
+                L /= n;
+                return L;
+            }
+            
+        }
+
+    }
+    static class RandomExtension {
+        /// <summary>Возвращает случайное double число в заданном диапазоне</summary>
+        /// <param name="random">Random экземпляр</param>
+        /// <param name="minValue">Начало диапазона</param>
+        /// <param name="maxValue">Конец диапазона</param>
+        /// <returns></returns>
+        static public double NextDouble(this Random random, double minValue = double.MinValue, double maxValue = double.MaxValue)
+            => random.NextDouble() * (maxValue - minValue) + minValue;
     }
 }
--- prog/labs/lab4_structure/structC#/obj/Debug/net8.0/structC#.AssemblyInfo.cs
+++ prog/labs/lab4_structure/structC#/obj/Debug/net8.0/structC#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("structC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae056bed47ece5dfab66e8959135b72dfb059033")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ac1146650be26a0ca40e31cd824b50a66ab3de")]
 [assembly: System.Reflection.AssemblyProductAttribute("structC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("structC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
