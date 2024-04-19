2c31db3134aadd1106892e0cdd2f3b90f7c49e73
erdswgtere <maxxim200508@gmail.com>
2024-04-19 22:07:09 +0300

    блок схемы

--- mathmodel/lab3/ConsoleApp2/ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ mathmodel/lab3/ConsoleApp2/ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7569379787f2143551a7f1758315abf8940fff65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e990aa5a8afe114ac1d3fc99534603b1fbdfa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
