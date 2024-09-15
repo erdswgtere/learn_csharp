8ef210c3857c16253e329e98806f1b297b3d694f
erdswgtere <maxxim200508@gmail.com>
2024-09-15 19:24:51 +0300

    программа многопоточной рекурсии готова

--- sys_prog/threads_lab1/taskrun18/taskrun18/Program.cs
+++ sys_prog/threads_lab1/taskrun18/taskrun18/Program.cs
@@ -3,12 +3,30 @@
 namespace taskrun18 {
     internal class Program {
         static void Main(string[] args) {
+            
             var stopwatch = new Stopwatch();
+            var tskrunrec = new TaskRunRecursion();
+            Console.Write("Введите сколько задач вы хотите запустить: ");
+            tskrunrec.Kol = ulong.Parse(Console.ReadLine()!);
             stopwatch.Start();
-            GetResult();
+            tskrunrec.GetResult();
+            stopwatch.Stop();
             long elapsedTime = stopwatch.ElapsedMilliseconds;
             Console.WriteLine($"Время выполнения: {elapsedTime} ms");
+            stopwatch.Reset();
+
+            var notskrunrec = new NoTaskRunRecursion();
+            stopwatch.Start();
+            var k = notskrunrec.RecSum();
+            Console.WriteLine($"Результат равен: {k}");
+            stopwatch.Stop();
+            elapsedTime = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"Время выполнения: {elapsedTime} ms");
+            
         }
+        
+    }
+    class NoTaskRunRecursion {
         static ulong Rec(ulong x) {
             if (x <= 18)
                 return x + 3;
@@ -17,7 +35,7 @@ namespace taskrun18 {
             else
                 return Rec(x - 1) + x * x + 5;
         }
-        static ulong RecSum() {
+       public ulong RecSum() {
             ulong k = 0;
             for (ulong i = 1; i <= 10000000; i++) {
                 bool ch = true;
@@ -35,16 +53,48 @@ namespace taskrun18 {
             }
             return k;
         }
-        static void GetResult() {
-            var TaskRec = new Task<ulong>(() => RecSum());
-            TaskRec.Start();
-            Console.WriteLine($"Результат вычислений {TaskRec.Result}");
+    }
+    class TaskRunRecursion {
+       static ulong kol;
+       static ulong k = 0;
+        public ulong Kol{
+            get => kol;
+            set => kol = value;
         }
-    }
-    class NoThreadRecursion {
-
-    }
-    class ThreadRecursion {
-
+       static ulong Rec(ulong x) {
+            if (x <= 18)
+                return x + 3;
+            else if (x > 18 && x % 3 == 0)
+                return x / 3 * Rec(x / 3) + x - 12;
+            else
+                return Rec(x - 1) + x * x + 5;
+        }
+        static bool RecSum(ulong StartCount) {
+            ulong i;
+            for (i = 0 + StartCount; i <= 10000000; i+=kol) {
+                bool ch = true;
+                ulong a = Rec(i);
+                while (a != 0) {
+                    if (a % 2 == 0) {
+                        a /= 10;
+                    }
+                    else {
+                        ch = false;
+                        break;
+                    }
+                }
+                if (ch == true) k++;
+            }
+            return true;
+        }
+        public void GetResult() {
+            var tasks = new List<Task<bool>>();
+            ulong strCount = 0;
+            for (uint ctr = 1; ctr <= kol; ctr++){
+                tasks.Add(Task.Run(() => RecSum(strCount++)));
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"Результат вычислений {k}");
+        }
     }
 }
